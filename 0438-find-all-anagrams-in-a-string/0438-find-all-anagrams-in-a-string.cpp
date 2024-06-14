6cf889739dc98089545c99af55ccee1bfae7aaaa
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-06-15 02:27:42 +0530

    Time: 22 ms (33.12%), Space: 35.2 MB (7.87%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        int n = s.size(), m = p.size();
+        // Initialize the frequency array for characters in p
+        vector<int> arr(26, 0);
+        
+        // Get the lengths of the strings s and p
+        int m = s.size();
+        int n = p.size();
+        
+        // Populate the frequency array with counts of characters in p
+        for(char &ch : p) {
+            arr[ch - 'a']++;
+        }
+        
+        // Initialize pointers and the result vector
+        int i = 0, j = 0;
         vector<int> result;
-        unordered_map<char, int> mp, ms;
-
-        // Build frequency map for characters in p
-        for (char &c : p) {
-            mp[c]++;
-        }
-
-        // Iterate over all possible starting positions of substrings in s
-        for (int i = 0; i <= n - m; i++) {
-            ms.clear();
-            // Build frequency map for the current substring
-            for (int j = i; j < i + m; j++) {
-                ms[s[j]]++;
+        
+        // Sliding window over string s
+        while(j < m) {
+            // Decrease the frequency of the current character in the window
+            arr[s[j] - 'a']--;
+            
+            // Check if the window size matches the length of p
+            if(j - i + 1 == n) {
+                // If all frequencies are zero, it's an anagram
+                if(arr == vector<int>(26, 0)) {
+                    result.push_back(i);
+                }
+                
+                // Restore the frequency of the character at the start of the window
+                arr[s[i] - 'a']++;
+                // Move the start of the window to the right
+                i++;
             }
-
-            // Compare frequency maps
-            if (mp == ms) {
-                result.push_back(i);
-            }
+            // Move the end of the window to the right
+            j++;
         }
-
+        
+        // Return the result vector containing starting indices of anagrams
         return result;
     }
 };
