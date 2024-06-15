89bdedd1bd82f68bdc1bfd7146735e575460982d
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-06-15 18:36:06 +0530

    Time: 131 ms (85.59%), Space: 81.7 MB (83.55%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        int n = profits.size();
+
+        vector<pair<int, int>> vec(n);
+
+        for (int i = 0; i < n; i++) {
+            vec[i] = {capital[i], profits[i]};
+        }
+
+        sort(vec.begin(), vec.end());  // sort based on capital
+
+        int i = 0;
+        priority_queue<int> pq;
+
+        while (k--) {
+            while (i < n && vec[i].first <= w) {
+                pq.push(vec[i].second);
+                i++;
+            }
+
+            if (!pq.empty()) {
+                w += pq.top();
+                pq.pop();
+            } else {
+                break;  // If no projects can be afforded, break the loop
+            }
+        }
+
+        return w;
+    }
+};
+
+// steps
+// 1.vec ->{{cap,profit}}
+// 2.sort based on cap
+// 3.iterate over vec and add in heap
