9f960c001a9504a47b107a9de4f9178e7538bc50
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-06-22 01:36:11 +0530

    Time: 0 ms (100%), Space: 8.9 MB (16.43%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     string largestGoodInteger(string num) {
         int n = num.size();
-        vector<pair<int, string>> vec; 
-        // Iterate through num to find all good integers of length 3
+        string res = "";
+        int maxNum = -1;
         for(int i = 0; i < n - 2; i++) {
             if(num[i] == num[i+1] && num[i+1] == num[i+2]) {
                 string goodInteger = num.substr(i, 3);
-                vec.push_back({num[i] - '0', goodInteger});
-                i += 2; // Skip to the next character after the current good integer
+                int currentNum = stoi(goodInteger);
+                if(currentNum > maxNum) {
+                    maxNum = currentNum;
+                    res = goodInteger; 
+                }
+                i += 2; 
             }
         }
-        if (vec.empty()) {
-            return ""; // Return an empty string if no good integers are found
-        }
-        sort(vec.begin(), vec.end());
-        return vec[vec.size() - 1].second; // Return the largest good integer string
+        return res;
     }
 };
