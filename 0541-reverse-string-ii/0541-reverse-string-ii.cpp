3642567d3a0db2b610e9b5d701c39bdb191cc92d
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-06-25 00:24:40 +0530

    Time: 3 ms (85.08%), Space: 8.4 MB (73.18%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.cpp
+++ 0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     string reverseStr(string s, int k) {
-        for(int i = k; i < s.size(); i += 2*k){
-            swap(s[i-1],s[i-k]);
+        int n = s.size();
+        // Calculate the end index for reversing
+        for(int i = 0; i < n; i += 2 * k){
+            int j = min(i + k - 1, n - 1);
+            reverse(s.begin() + i, s.begin() + j + 1);
         }
         return s;
-        
     }
 };
