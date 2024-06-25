5fe79ede240b3a12a773d3f626f09cdbf3d11815
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-06-25 13:24:21 +0530

    Time: 5 ms (31.24%), Space: 10 MB (59.62%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        
+        set<int> s;
+
+        for(auto &i : nums){
+            if(i > 0){
+                s.insert(i);
+            }
+        }
+        return s.size();
+    }
+};
+
+// observation :- unique number pe hi operation performed hogaa
