a4b91233b0b818459b8bc6a7a171d475371be6f4
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-06-27 05:27:15 +0530

    Time: 8 ms (51.02%), Space: 14 MB (35.66%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,40 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        vector<vector<int>> ans;
-
-        int i = 0, j = 0;
-        int n = nums1.size();
-        int m = nums2.size();
-
-        while (i < n || j < m) {
-            if (i == n) {
-                // All elements of nums1 are finished, add remaining elements from nums2
-                ans.push_back({nums2[j][0], nums2[j][1]});
-                j++;
-            } else if (j == m) {
-                // All elements of nums2 are finished, add remaining elements from nums1
-                ans.push_back({nums1[i][0], nums1[i][1]});
-                i++;
-            } else {
-                // Both arrays have elements left to process
-                if (nums1[i][0] == nums2[j][0]) {
-                    // IDs are the same, sum their values and add to result
-                    ans.push_back({nums1[i][0], nums1[i][1] + nums2[j][1]});
-                    i++;
-                    j++;
-                } else if (nums1[i][0] < nums2[j][0]) {
-                    // Add the element from nums1 with the smaller ID
-                    ans.push_back({nums1[i][0], nums1[i][1]});
-                    i++;
-                } else {
-                    // Add the element from nums2 with the smaller ID
-                    ans.push_back({nums2[j][0], nums2[j][1]});
-                    j++;
-                }
-            }
+        // Map to store the cumulative sums of values by ID
+        map<int, int> count;
+        
+        // Insert values from nums1 into the map
+        for (const auto& x : nums1) {
+            count[x[0]] += x[1];
         }
-
-        return ans;
+        
+        // Insert values from nums2 into the map
+        for (const auto& x : nums2) {
+            count[x[0]] += x[1];
+        }
+        
+        // Prepare the result vector
+        vector<vector<int>> result;
+        for (const auto& x : count) {
+            result.push_back({x.first, x.second});
+        }
+        
+        return result;
     }
 };
