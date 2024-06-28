55ff4a64a55d8d75667bb7da1110b641f067c9d7
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-06-28 21:41:07 +0530

    Time: 9 ms (30.1%), Space: 26.3 MB (30.1%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     bool isGood(vector<int>& nums) {
-        // Sort the input vector nums
-        sort(nums.begin(), nums.end());
-        
-        int n = nums.size(); // Get the size of nums
-        vector<int> vec(n); // Create a new vector vec of size n
-        
-        // Copy elements from sorted nums to vec
-        for (int i = 0; i < n; i++) {
-            vec[i] = nums[i];
+        sort(nums.begin(),nums.end());
+
+        int a = *max_element(nums.begin(),nums.end());
+
+        vector<int> vec;
+
+        for(int i = 1; i <= a; i++){
+            vec.push_back(i);
+        }
+        vec.push_back(a);
+
+        if(nums == vec){
+            return true;
         }
 
-        // Determine the maximum element in nums
-        int a = *max_element(nums.begin(), nums.end());
-        int b = a + 1;
-
-        // Check if b is not equal to a
-        if (b != n) {
-            return false; // Return false if b is not equal to a
-        } else {
-            // Compare vec with sorted nums
-            for (int i = 0; i < n; i++) {
-                if (vec[i] != nums[i]) {
-                    return false;
-                }
-            }
-            return true; // Return true if vec matches sorted nums
-        }
+        return false;
+        
     }
 };
