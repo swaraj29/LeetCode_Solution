3165ed166c0b0d544cc0dccb123b39b5e8c1ba32
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-07-01 14:25:05 +0530

    Time: 10 ms (46.93%), Space: 24.3 MB (92.19%) - LeetHub

---
+++ 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <bitset>
+using namespace std;
+
+class Solution {
+public:
+    int sumIndicesWithKSetBits(vector<int>& nums, int k) {
+        int s = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            bitset<32> p(i);
+
+            if(p.count() == k){
+                s += nums[i];
+            }
+        }
+        return s;
+    }
+};
