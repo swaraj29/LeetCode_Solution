93691c9fb60dd626bd075540e7bec68e1879b83a
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-07-02 12:59:36 +0530

    Time: 8 ms (65.99%), Space: 19.6 MB (16.78%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-
         int n = nums.size();
 
-        int ea = n / 3;
-        int c = 1;
-
         vector<int> vec;
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
-                c++;
-            } else {
-                if (c > ea) {
-                    vec.push_back(nums[i]);
-                }
-                c = 1;
-            }
-        }
-        
-        // Check the last element
-        if (c > ea) {
-            vec.push_back(nums[n - 1]);
+
+        unordered_map<int,int> mp;
+
+        for(auto i : nums){
+            mp[i]++;
         }
 
-        return vec;
+        for(auto k : mp){
+            if(k .second > n/3)
+            vec.push_back(k.first);
+        } 
+        return vec; 
     }
 };
