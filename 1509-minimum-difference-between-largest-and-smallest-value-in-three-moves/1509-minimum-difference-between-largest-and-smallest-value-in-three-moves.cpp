83bd4f0e19707c0e557d1b9d951e045d262b3b33
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-07-04 05:03:36 +0530

    Time: 70 ms (49.88%), Space: 38.6 MB (79.26%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    int minDifference(vector<int>& nums) {
-        sort(begin(nums), end(nums));
-        int n = nums.size();
-        if(n <= 4) {
-            return 0;
-        }
-        int result = INT_MAX;
-        result = min(result, nums[n-4] - nums[0]);
-        result = min(result, nums[n-1] - nums[3]);
-        result = min(result, nums[n-3] - nums[1]);
-        result = min(result, nums[n-2] - nums[2]);
+    int minDifference(vector<int>& nums) 
+    {
+        if(nums.size() <= 3) return 0;
+        sort(nums.begin(), nums.end());
 
-        return result;
+        int ans = INT_MAX, n = nums.size();
+
+        // frist three remove
+        ans = min(ans, nums.back() - nums[3]);
+
+        // last three remove
+        ans = min(ans, nums[n-4] - nums[0]);
+
+        // first 2 remove, last 1 remove
+        ans = min(ans, nums[n-2] - nums[2]);
+
+        // first 1 remove, last 2 remove
+        ans = min(ans, nums[n-3] - nums[1]);
+
+        return ans;
     }
 };
