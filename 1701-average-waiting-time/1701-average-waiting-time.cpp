155b935be13096795a6451b823cbab3fedc35503
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-07-09 20:48:13 +0530

    Time: 143 ms (76.47%), Space: 82 MB (17.65%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.cpp
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    double averageWaitingTime(vector<vector<int>>& customers) {
+    double averageWaitingTime(std::vector<std::vector<int>>& customers) {
         
-        vector<int> aw;
+        std::vector<long long> aw;  // Change vector<int> to vector<long long>
         int r = customers.size();
-        int s = 0;
+        int c = customers[0].size();
+        long long s = 0;  // Change int to long long
 
         for (int i = 0; i < r; i++) {
             int a = customers[i][0];
-            int t = customers[i][1];
-            int f, w;
-            
+            int t = customers[i][1];  // Move t assignment out of the inner loop
             if (a > s) {
-                f = a + t;
+                long long f = static_cast<long long>(a) + t;  // Use long long for f
                 s = f;
+                long long w = f - a;
+                aw.push_back(w);
             } else {
-                f = s + t;
+                long long f = s + t;  // Use long long for f
+                long long w = f - a;
                 s = f;
+                aw.push_back(w);
             }
-            
-            w = f - a;
-            aw.push_back(w);
         }
-        double p = accumulate(aw.begin(), aw.end(), 0LL);
-        p = p/aw.size();
-        return p;     
+        
+        long long p = std::accumulate(aw.begin(), aw.end(), 0LL);  // Use 0LL for long long initial value
+        return static_cast<double>(p) / aw.size();        
     }
 };
