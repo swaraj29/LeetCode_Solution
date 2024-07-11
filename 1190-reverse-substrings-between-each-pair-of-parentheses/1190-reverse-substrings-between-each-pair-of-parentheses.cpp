5458ccd5f2427683e36ab70b556e749b6f60b3a2
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-07-12 01:30:51 +0530

    Time: 0 ms (100%), Space: 7.7 MB (51.94%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     string reverseParentheses(string s) {
-        stack<int> st;
+        vector<int> open_brackets;
 
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] == '('){
-                st.push(i);
-            }
-            else if(s[i] == ')'){
-                int start = st.top();
-                st.pop();
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                open_brackets.push_back(i);  // Store the position of '('
+            } else if (s[i] == ')') {
+                int start = open_brackets.back();
+                open_brackets.pop_back();
                 reverse(s.begin() + start + 1, s.begin() + i);
             }
         }
+
         // Remove all parentheses from the string
         string result;
-        for(char c : s){
-            if(c != '(' && c != ')'){
+        for (char c : s) {
+            if (c != '(' && c != ')') {
                 result += c;
             }
         }
+
         return result;
     }
 };
