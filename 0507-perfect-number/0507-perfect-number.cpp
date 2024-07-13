1b26af31c53a5bce14728ea4bff8a3e5c271b590
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-07-13 19:35:06 +0530

    Time: 1035 ms (53.37%), Space: 7.3 MB (7.91%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -3,7 +3,7 @@ public:
     bool checkPerfectNumber(int num) {
         
         int sum = 0;
-        for(int i = 1; i < num; i++){
+        for(int i = 1; i <= num/2; i++){
             if(num % i == 0){
                 sum += i;
             }
