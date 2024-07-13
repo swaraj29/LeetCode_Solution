24d95ae852e25eb9bbf538375f02bfdb8f0cbfd9
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-07-13 18:03:52 +0530

    Time: 3 ms (44.27%), Space: 7.5 MB (20.51%) - LeetHub

--- 0492-construct-the-rectangle/0492-construct-the-rectangle.cpp
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
-    std::vector<int> constructRectangle(int area) {
-        int w = sqrt(area);
-        std::vector<int> result;
-        
-        for (int width = w; width >= 1; --width) {
-            if (area % width == 0) {
-                int length = area / width;
-                result.push_back(length);
-                result.push_back(width);
-                break;  // Found the minimum difference pair, no need to continue
-            }
+    vector<int> constructRectangle(int area) {
+        int m = sqrt(area);
+        while(area % m != 0){
+            m--;
         }
-        
-        return result;
+        return {area/m,m}; 
     }
 };
