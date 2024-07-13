db1f7b3a71cdb18b7d346d2c4c3d46e647309306
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-07-13 19:28:41 +0530

    Time: 2075 ms (23.23%), Space: 7.4 MB (7.91%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        
+        int sum = 0;
+        for(int i = 1; i < num; i++){
+            if(num % i == 0){
+                sum += i;
+            }
+        }
+        if(sum == num){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
