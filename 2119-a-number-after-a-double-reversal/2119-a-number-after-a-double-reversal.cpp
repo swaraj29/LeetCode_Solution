39d8d38a63c4c392644a8741c3050e1e565764e3
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-07-14 23:30:32 +0530

    Time: 0 ms (100%), Space: 7.4 MB (19.23%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     bool isSameAfterReversals(int num) {
-        if(num == 0){
-            return true;
-        }
-        if(num % 10 != 0){
-            return true;
-        }
-        return false;
+        return (num == 0 || num % 10 != 0) ? true : false;
     }
 };
