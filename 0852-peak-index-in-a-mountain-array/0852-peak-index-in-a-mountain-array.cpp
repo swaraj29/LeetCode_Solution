00ce4a4e67fba01318d9e5c134dc5ed26da3fd88
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-08-25 00:35:40 +0530

    Time: 78 ms (68.21%), Space: 62.3 MB (38.32%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        
         int start = 0;
         int end = arr.size() - 1;
 
-        while(start <= end){
-            int mid = end + (start - end)/2;
+        while(start < end){
+            int mid = start + (end - start) / 2;
 
-            // peak element
-            if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]){
-                return mid;
-            }
-            // Right side Move
-            else if(arr[mid] > arr[mid - 1]){
+            // If mid is on the rising slope
+            if(arr[mid] < arr[mid + 1]){
                 start = mid + 1;
             }
-            // Left side Move
+            // If mid is on the falling slope
             else{
-                end = mid - 1;
+                end = mid;
             }
         }
-        return -1;
+
+        // Both start and end should point to the peak when the loop exits
+        return start;
     }
 };
