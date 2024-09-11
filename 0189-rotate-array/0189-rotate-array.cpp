d7c870c33286039e4f7d85e7f74abd69c57cd423
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-09-11 22:10:41 +0530

    Time: 17 ms (80.88%), Space: 28 MB (33.68%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        vector<int> temp(n);
+
+        for(int i = 0; i < n; i++){
+            temp[ (i + k) % n ] = nums[i];
+        }
+        // copy temp into num vector
+        nums = temp;
+    }
+};
