67f4b76cc01d09a36aed4ebada167a9ac1e45b4c
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-09-16 22:18:22 +0530

    Time: 144 ms (91.11%), Space: 116.7 MB (92.03%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -11,30 +11,61 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        
-        vector<int> vec;
-        ListNode* curr = head;
+        // If the list has only one node, it's always a palindrome
+        if (head->next == NULL) {
+            return true;
+        }
 
-        while(curr != NULL){
-            vec.push_back(curr->val);
+        // Step 1: Calculate the length of the linked list
+        ListNode* temp = head;
+        int count = 0;
+        
+        // Traverse the list to count the number of nodes
+        while (temp != NULL) {
+            count++;
+            temp = temp->next;
+        }
+        
+        // Divide the count by 2 to find the midpoint
+        count /= 2;
+
+        // Step 2: Reverse the second half of the list
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        
+        // Traverse the list until the midpoint
+        while (count--) {
+            prev = curr;
             curr = curr->next;
         }
 
-        int i = 0;
-        int j = vec.size() - 1;
-        bool flag = false;
-        while(i < j){
-            if(vec[i] == vec[j]){
-                flag = true; 
-                i++;
-                j--;
-            }
-            else{
-                flag = false;
-                break;
-            }
+        // Disconnect the first half from the second half of the list
+        prev->next = NULL;
+
+        // Now reverse the second half of the list
+        ListNode* front = NULL;
+        prev = NULL;
+        while (curr) {
+            front = curr->next;    // Store the next node
+            curr->next = prev;     // Reverse the link
+            prev = curr;           // Move prev to the current node
+            curr = front;          // Move to the next node in the list
         }
 
-        return flag ? true : false;  
+        // Step 3: Compare the first half and the reversed second half
+        ListNode* head1 = head; 
+        ListNode* head2 = prev;   
+        
+        // Traverse and compare each node in both halves
+        while (head1) {
+            if (head1->val != head2->val) {
+                return false;     
+            }
+            head1 = head1->next;  
+            head2 = head2->next;  
+        }
+
+        // If all values match, return true (it is a palindrome)
+        return true;
     }
 };
