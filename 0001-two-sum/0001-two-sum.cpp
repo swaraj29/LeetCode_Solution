12e10dba5624119e6ff0529c96e9020b84e5f4b7
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-12-12 04:43:08 +0530

    Time: 3 ms (78.16%), Space: 14.8 MB (19.24%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
+        
+        unordered_map<int,int> mp;
 
-        for (int i = 0; i < nums.size(); i++) {
+        for(int i = 0; i < nums.size(); i++){
             int comp = target - nums[i];
 
-            // Check if the complement exists in the map
-            if (mp.find(comp) != mp.end()) {
-                return {mp[comp], i};
+            if(mp.find(comp) != mp.end()){
+                return {mp[comp],i};
             }
 
-            // Store the current number and its index in the map
             mp[nums[i]] = i;
         }
-
-        // If no solution is found, return an empty vector (or handle the error)
         return {};
     }
 };
